0788b91b96d27a1e4c1910944218480c5896a8ae
fabio1008 <fabiocarini2003@gmail.com>
2022-05-12 23:56:34 -0300

    template.

--- devpro/reduce/views.py
+++ devpro/reduce/views.py
@@ -6,6 +6,16 @@ from django.shortcuts import render, redirect
 from devpro.reduce.models import UrlRedirect
 
 
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
+    }
+    return render(requisicao, 'reduce/relatorio.html', contexto)
+
+
 def redirecionar(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     return redirect(url_redirect.destino)
